077600f33ee5774f9a01e56b35b9c85e2c36a083
Luis Luvia <luluvia@asu.edu>
2019-12-05 23:48:00 -0700

    1.0

--- src/main/java/me/luyu/cse360/Scribe.java
+++ src/main/java/me/luyu/cse360/Scribe.java
@@ -16,7 +16,7 @@ public class Scribe extends Application
 
     private static Scene scene;
     private static Stage pStage;
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0";
     public static boolean DEBUG = true;
 
     @Override
